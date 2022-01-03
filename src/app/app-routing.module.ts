0949733741952ebaa052790d1305f318eb55c74a
JuanGarcia0162 <juadgarca@misena.edu.co>
2022-01-03 18:18:24 -0500

    Creacion del primer componente de prueba para las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ExampleComponent } from './example/example.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', //TODO = localhost:4200/
+    component:ExampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
