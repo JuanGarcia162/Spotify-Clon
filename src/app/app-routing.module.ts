495bd9cfb50b6e02aebc38289cb760cd0b4b1793
JuanGarcia0162 <juadgarca@misena.edu.co>
2022-01-03 19:04:31 -0500

    Implementacion de LazyLoading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { ExampleComponent } from './example/example.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '', //TODO = localhost:4200/
-    component:ExampleComponent
+    path: '', //TODO: localhost:4200/
+    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule)
   }
 ];
 
