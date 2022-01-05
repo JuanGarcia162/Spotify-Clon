4a645722bd3639addf94eb01cd03d0b2009c339d
JuanGarcia0162 <juadgarca@misena.edu.co>
2022-01-05 18:03:25 -0500

    Creacion y estructura de 2 interface; actualizacion del mockCover con los datos de la interface 'tracks.model

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TracksModels } from '@core/models/tracks.model';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-media-player',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover: any = {
+  mockCover: TracksModels = {
     cover: 'https://cdns-images.dzcdn.net/images/cover/ba5eaf2f3a49768164d0728b7ba64372/500x500.jpg',
     album: 'AC/DC',
-    name: 'T.N.T. (Live At River Plate, December 2009)'
+    name: 'T.N.T. (Live At River Plate, December 2009)',
+    url: 'http://localhost/track.mp3',
+    _id: 1
   }
   constructor() { }
 
