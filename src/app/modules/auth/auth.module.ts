7feeb73b24d3432107ddb6a79df2091da13fa2ed
JuanGarcia0162 <juadgarca@misena.edu.co>
2022-01-16 01:27:33 -0500

    Refactorizacion de nombres

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { LoginRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
   declarations: [
-    AuthPageComponent
+    LoginPageComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    LoginRoutingModule
   ]
 })
-export class AuthModule { }
+export class LoginModule { }
