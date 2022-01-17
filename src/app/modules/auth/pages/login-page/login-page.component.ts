0f2822660a35f21d71cf51fb295b19be6c3a4bcb
JuanGarcia0162 <juadgarca@misena.edu.co>
2022-01-17 18:40:25 -0500

    Envio de informacion a media player

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -8,7 +9,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
-  constructor() { }
+
+  constructor(private auhtService: AuthService) { }
 
   ngOnInit(): void {
 
@@ -29,8 +31,8 @@ export class LoginPageComponent implements OnInit {
 
   sendLogin(): void
   {
-    const body = this.formLogin.value
-    console.log('🤞🤞', body);
+    const {email, password} = this.formLogin.value
+    this.auhtService.sendCredentials(email,password)
   }
 
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
   mockCover: TrackModel = {
     cover: 'https://t2.genius.com/unsafe/327x327/https%3A%2F%2Fimages.genius.com%2F59ccba75136f90d6a75072cf1c9ee3e5.1000x1000x1.png',
     album: 'BZRP Music Sessions',
@@ -14,9 +16,26 @@ export class MediaPlayerComponent implements OnInit {
     url: 'http://localhost/track.mp3',
     _id: 1
   }
-  constructor() { }
+
+  listObservers$: Array<Subscription> = [
+
+  ]
+
+  constructor(private multimediaService: MultimediaService) { }
 
   ngOnInit(): void {
+    const observer1$: Subscription = this.multimediaService.callback.subscribe(
+      (response:TrackModel) => {
+        console.log('Recibiendo Cancion', response);
+      }
+    )
+
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe)
+      console.log('🎈🎈🎈🎈🎈🎈🎈, BOOM!')
   }
 
 }
